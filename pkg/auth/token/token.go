62800a92a54a18c31ff1fcca768c64a405bf51bf
Borisa Krstic <borisakrstic02@gmail.com>
2022-08-23 17:20:46 +0200

    Added expiration date to JWT

--- pkg/auth/token/token.go
+++ pkg/auth/token/token.go
@@ -2,6 +2,7 @@ package token
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/golang-jwt/jwt"
 	"github.com/spf13/viper"
@@ -15,7 +16,8 @@ func GenerateToken(id string) (string, error) {
 	// Create a new token object, specifying signing method and the claims
 	// you would like it to contain.
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"id": id,
+		"id":  id,
+		"exp": time.Now().Add(time.Hour * 24).Unix(),
 	})
 
 	// Sign and get the complete encoded token as a string using the secret
