fe07f44e5b8ccecb6ead0e1bd96b7ba9999634e9
Borisa Krstic <borisakrstic02@gmail.com>
2022-08-25 23:27:09 +0200

    Insert user role with seeder

--- pkg/common/db/seeds/seeder.go
+++ pkg/common/db/seeds/seeder.go
@@ -1,6 +1,9 @@
 package seeds
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/borisa99/gin-starter/pkg/common/models"
 	"gorm.io/gorm"
 )
@@ -14,28 +17,41 @@ func Plant(db *gorm.DB) {
 		{Name: "admin"},
 		{Name: "base"},
 	}
-	users := []models.User{
-		{FirstName: "admin",
-			LastName:     "admin",
-			Email:        "admin@mail.com",
-			PasswordHash: "$2a$10$npPMoi7q5PWy3RBZtI.zteisiP28MYP0cttG35Hqsqj9gB74uv54S"},
+
+	values, err := Seeder(db, models.Role{}, roles)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(values)
+		users := []models.User{
+			{FirstName: "admin",
+				LastName:     "admin",
+				Email:        "admin@mail.com",
+				PasswordHash: "$2a$10$npPMoi7q5PWy3RBZtI.zteisiP28MYP0cttG35Hqsqj9gB74uv54S",
+				Roles: []models.UserRole{
+					{RoleId: values[0].ID},
+				},
+			},
+		}
+		Seeder(db, models.User{}, users)
+
 	}
-	Seeder(db, models.Role{}, roles)
-	Seeder(db, models.User{}, users)
 
 }
 
-func Seeder[M Model](db *gorm.DB, model M, values []M) {
+func Seeder[M Model](db *gorm.DB, model M, values []M) ([]M, error) {
 	ok := db.Migrator().HasTable(&model)
 	if !ok {
-		return
+		return nil, errors.New("table does not exist")
 	}
 
 	var exists bool
 	db.Model(&model).Select("count(*) > 0").Find(&exists)
 
 	if !exists {
-
 		db.Create(&values)
+		return values, nil
 	}
+
+	return nil, errors.New("rows already exist")
 }
